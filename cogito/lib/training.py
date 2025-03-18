b5c0df4b23c141c8e211a27731316a84d99a03bd
Sergio Tejón <sergio.tejon@gmail.com>
2025-03-18 11:59:37 +0100

    refactor: implement SDK classes for prediction and training
    - Introduce `Predict` and `Trainer` classes to encapsulate prediction and training logic, respectively.
    - Refactor the `run` method into instance methods for better organization and clarity.
    - Enhance error handling by raising `NoSetupMethodError` when setup methods are not found.
    - Streamline the setup process for both classes, allowing for more flexible usage in user code.

--- cogito/lib/training.py
+++ cogito/lib/training.py
@@ -1,30 +1,39 @@
 from cogito.core.config.file import build_config_file
 from cogito.core.utils import instance_class
+from cogito.core.exceptions import NoSetupMethodError
 
 
-def run(config_path, payload_data, run_setup=True):
-    """
-    Train a model using the payload data
-    """
+# SDK Trainer class
+class Trainer:
 
-    config = build_config_file(config_path)
-    trainer = instance_class(config.cogito.get_trainer)
+    # Initialize trainer
+    def __init__(self, config_path):
+        # Build config
+        config = build_config_file(config_path)
 
-    # Run setup if needed
-    try:
-        if (
-            hasattr(trainer, "setup")
-            and callable(getattr(trainer, "setup"))
-            and run_setup
-        ):
-            trainer.setup()
-    except Exception as e:
-        raise Exception(f"Error setting up the trainer: {e}")
+        # Get trainer path and instance
+        self.config_path = config_path
+        self.trainer = instance_class(config.cogito.get_trainer)
 
-    # Call train method with payload data
-    try:
-        result = trainer.train(**payload_data)
-    except Exception as e:
-        raise Exception(f"Error training the model: {e}")
+    # Setup trainer calling setup method in the user's code
+    def setup(self):
+        # Run setup if needed
+        try:
+            if hasattr(self.trainer, "setup") and callable(
+                getattr(self.trainer, "setup")
+            ):
+                self.trainer.setup()
+            else:
+                raise NoSetupMethodError(self.trainer.__class__.__name__)
+        except Exception as e:
+            raise Exception(f"Error setting up the trainer: {e}")
 
-    return result
+    # Run training calling train method in the user's code
+    def run(self, payload_data, run_setup=True):
+        # Call train method with payload data
+        try:
+            result = self.trainer.train(**payload_data)
+        except Exception as e:
+            raise Exception(f"Error training the model: {e}")
+
+        return result
