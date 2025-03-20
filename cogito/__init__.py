531c8d8cb12a2c41558d4deb6a44913c1ff6e05d
Sergio Tejón <sergio.tejon@gmail.com>
2025-03-20 13:20:53 +0100

    feat: add versioning support with _version.py module
    - Introduce a new _version.py file to manage package versioning.
    - Update __init__.py to import the package version, enhancing visibility of the current version in the library.

--- cogito/__init__.py
+++ cogito/__init__.py
@@ -2,6 +2,8 @@ from cogito.core.app import Application
 from cogito.core.models import BasePredictor
 from cogito.core.utils import model_download
 
+from ._version import __version__
+
 __all__ = [
     "Application",
     "BasePredictor",
